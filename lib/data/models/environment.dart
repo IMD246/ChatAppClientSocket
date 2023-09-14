54096d9e8278799b5e520434c119cf4315ff48f2
Thanh Duy <thanhduy@main99.com>
2023-09-14 10:11:44 +0700

    up url network

--- lib/data/models/environment.dart
+++ lib/data/models/environment.dart
@@ -3,8 +3,8 @@ class Environment {
   late String apiURL;
   Environment({required bool isProduct}) {
     if (isProduct) {
-      baseURL = "https://chatappsocket.onrender.com";
-      apiURL = "https://chatappsocket.onrender.com/api/";
+      baseURL = "https://chatappsocket2.onrender.com";
+      apiURL = "https://chatappsocket2.onrender.com/api/";
     } else {
       baseURL = "http://192.168.40.1:5000";
       apiURL = "http://192.168.40.1:5000/api/";
